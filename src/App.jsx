849a34889e5c45ad8af2f49bcf0366313182fd3d
hmntk <hemanth@gameopedia.com>
2024-08-05 20:51:29 -0400

    first up

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>App to be hosted serverless on AWS Cloud</h1>
+      <div className="card">
+        <h3>Click btn below to test the state</h3>
+        <button onClick={() => setCount((count) => count + 1)}>
+          Current state {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        <i><b>Ok. I did that</b></i>
+      </p>
+    </>
+  )
+}
+
+export default App
